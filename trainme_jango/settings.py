b9ef6070f4fac18bd534987dab3d4005137a25f5
Nkechi Ugwu <ugwusophronia@gmail.com>
2023-11-16 21:37:17 -0500

    settings updated

--- trainme_jango/settings.py
+++ trainme_jango/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jango_projbase',
         'USER': 'root',
-        'PASSWORD': 'NwunyeEdu20$',
+        'PASSWORD': 'xxxx',
         'HOST':'localhost',
         'PORT':'3306',
     }
